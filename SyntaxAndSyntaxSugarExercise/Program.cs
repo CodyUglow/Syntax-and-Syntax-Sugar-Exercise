58f289410cbd104df455294c517e4e73b417ba99
CodyUglow <blackheart3621@gmail.com>
2025-08-06 12:43:25 -0500

    Did as was specified in the exercise instructions.

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Inferred code snippet
+            var answer = 4;
+            string response;
+
+            //interpolated code snippet
+            if (answer < 9)
+            {
+                response = $"{answer} is less than nine.";
+            }
+            else
+            {
+                response = $"{answer} is greater than or equal to nine";
+            }
+            // Ternary operator code snippet
+            answer = 4;
+            response = answer < 9
+                ? $"{answer} is less than nine."
+                : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
         }
     }
 }
